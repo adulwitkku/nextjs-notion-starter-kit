7f40823d21ba092865be9a8d5ed4fe298c09074e
adulwitkku <adulwit@kkumail.com>
2025-11-09 00:53:26 +0700

    ga and clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,35 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+
+            {/* Google Analytics */}
+            <script
+              async
+              src='https://www.googletagmanager.com/gtag/js?id=G-1ZV6T5F8YG'
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-1ZV6T5F8YG');
+                `
+              }}
+            />
+
+            {/* Microsoft Clarity */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  })(window, document, "clarity", "script", "l968noaewt");
+                `
+              }}
+            />
           </Head>
 
           <body>
